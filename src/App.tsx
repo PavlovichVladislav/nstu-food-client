88651f15272412ed596ba767a0df1099e3575a02
PavlovichVladislav <jeffry2814@gmail.com>
2023-03-26 14:19:46 +0700

    made Categories component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Header from "./components/header/Header";
-
+import Categories from "./components/categories/Categories";
 
 import "normalize.css";
-import './scss/index.scss';
+import "./scss/index.scss";
 
 function App() {
    return (
       <div className="wrapper">
          <div className="container">
             <Header />
+            <section className="content__top">
+               <Categories />
+            </section>
          </div>
       </div>
    );
---
+++ src/components/categories/Categories.tsx
@@ -0,0 +1,21 @@
+import s from "./Categories.module.scss";
+
+const categories = ['Все'];
+
+for (let i = 1; i <= 8; i++) {
+    categories.push(`Корпус ${i}`)
+}
+
+const Categories = () => {    
+   return (
+      <div className={s.categories}>
+         <ul>
+            {categories.map((categoryName, i) => (
+                <li className={i === 0 ? `${s.active}` : ''}>{categoryName}</li>
+            ))}
+         </ul>
+      </div>
+   );
+};
+
+export default Categories;

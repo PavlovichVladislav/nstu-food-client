786e099b85aaa53b90ae2bb6f1bcb2f9d1a637f8
PavlovichVladislav <jeffry2814@gmail.com>
2023-05-22 11:26:55 +0700

    fixed a server error that distributes static files

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const path = require('path');
+
+const PORT = process.env.PORT ?? 8000;
+
 const app = express();
 
-
-
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'build'));
-
+app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, 'build')));
-app.use(express.urlencoded({extended: true}));
 
-const port = process.env.PORT ?? 8000;
-
-app.get('/', async(res, req, next) => {
+app.get("*", async(req, res, next) => {
     const dirPath = path.join(__dirname, 'build', 'index.html');
-    console.log(dirPath);
-    res.render(dirPath);
+    res.sendFile(dirPath);
 })
 
-app.listen(port, () => console.log(`Server working on port ${port}`));
+app.listen(PORT, () => console.log(`Server working on port ${PORT}`));
